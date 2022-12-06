db50abfe8ac2526db75b1ce7ef45e95e47b87fb7
mjiw <113112060+mjiw@users.noreply.github.com>
2022-12-06 16:32:24 +0900

    test 2

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct Book{
+	int number;
+	char title[10];	
+}; 
+
+int main(int argc, char *argv[]) {
+	struct Book *p;
+	
+	p=(struct Book*)malloc(2*sizeof(struct Book));
+	
+	p->number=1;
+	strcpy(p->title,"C Programming");
+	(p+1)->number=2;
+	strcpy((p+1)->title,"Electronics");
+
+	free(p);
+	
+	return 0;
+}
